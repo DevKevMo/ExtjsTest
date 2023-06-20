5bc6adca63f5f87364c6fb5758fd79335e21c138
K.Lab <Kevin-Moritz-2001@outlook.de>
2023-06-20 09:42:04 +0200

    Login update with SessionId

--- app/comp/login/LoginController.js
+++ app/comp/login/LoginController.js
@@ -2,7 +2,25 @@ Ext.define("test.comp.login.LoginController", {
   extend: "Ext.app.ViewController",
   alias: "controller.login",
 
-  onLoginClick: function () {
-    debugger;
+  onLoginClick: function (btn, value) {
+    form = this.lookup("form"); // formfield with the login values (username and password)
+    formValues = form.getValues();
+    Ext.Ajax.request({
+      url: "app/api/main.cfc?method=login",
+      method: "POST",
+      params: {
+        userData: Ext.encode(formValues),
+      },
+      success: (response) => {
+        var data = Ext.decode(response.responseText); // Get the values ERROR(true,false), DATA(returnValue), and MESSAGE
+        Ext.toast(data.MESSAGE);
+        if (!data.ERROR) {
+          localStorage.setItem("userSessionKm", data.DATA);
+          this.getView().destroy();
+        } else {
+          form.reset();
+        }
+      },
+    });
   },
 });
